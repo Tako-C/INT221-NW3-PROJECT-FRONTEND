a9ca0582894f37437bb430c1991bf2d5b238cad4
Tako-c <phakphum2phrajrern@gmail.com>
2024-05-02 20:50:49 +0700

    Fix:function

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import taskTable from "../components/taskTable.vue"
 import taskDetail from "../components/taskDetail.vue"
+import taskAdd from "@/components/taskAdd.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,11 @@ const router = createRouter({
         {
             path: "/task",
             name: "taskTable",
-            component: taskTable, children:[{ path: ":id", component: taskDetail}],
+            component: taskTable, children:[{ path: ":id", component: taskDetail},
+        {
+            path: "add",
+            component: taskAdd
+        }]
         },
         // {
         //     path: "/",
