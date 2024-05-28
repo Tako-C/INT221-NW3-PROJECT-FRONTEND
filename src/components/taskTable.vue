1ed0e439b27e8c2b400d0e0c0027fa33cb1387dc
Tako-c <phakphum2phrajrern@gmail.com>
2024-05-28 20:21:08 +0700

    Fix Ui and function clear all filter

--- src/components/taskTable.vue
+++ src/components/taskTable.vue
@@ -30,6 +30,8 @@ async function fetchData() {
       'tasks?sortBy=status.name&FilterStatuses=' +
       filterList.value.map((status) => status.trim()).join('&FilterStatuses=')
     taskData.value = await getData(endpointFilter)
+    taskData.value.sort((a, b) => a.id - b.id)
+    
   } else {
     if (sortStatus.value === 1) {
       endpoint += '?sortBy=status.name&FilterStatuses'
@@ -119,7 +121,7 @@ function addFilter() {
     newFilterString.value = ''
     fetchData()
     showStatusList.value = false
-    sortStatus.value = 1
+    // sortStatus.value = 1
   } else {
     window.alert(newFilterString.value + ' is current filter.')
     showStatusList.value = false
@@ -134,6 +136,11 @@ function filteredStatuses() {
 
 function removeFilter(index) {
   filterList.value.splice(index, 1)
+  fetchData()
+}
+
+function removeAllFilter(){
+  filterList.value = []
   fetchData()
 }
 
@@ -211,7 +218,7 @@ onMounted(fetchData)
                 placeholder="Search Filter something . . ."
                 v-model="newFilterString"
                 @keyup.enter="addFilter"
-                class="w-96"
+                class="itbkk-status-filter w-96"
               />
               <img
                 src="https://www.svgrepo.com/show/46113/magnifying-glass.svg"
@@ -248,13 +255,19 @@ onMounted(fetchData)
             :key="index"
             class="flex justify-center items-center rounded-lg bg-slate-300 w-auto m-2 p-2"
           >
-            <p>{{ filter }}</p>
+            <p class=" itbkk-filter-item">{{ filter }}</p>
             <img
               src="https://www.svgrepo.com/show/21045/delete-button.svg"
               alt="Delete"
-              class="ml-2 cursor-pointer w-4 h-4"
+              class="itbkk-filter-item-clear ml-2 cursor-pointer w-4 h-4"
               @click="removeFilter(index)"
             />
+          </div>
+          <div v-show="filterList.length > 0" @click="removeAllFilter()" class="itbkk-filter-item-clear">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer text-red-500">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+
           </div>
         </div>
 
@@ -282,7 +295,7 @@ onMounted(fetchData)
             <th>Assignees</th>
             <th class="flex items-center justify-between pt-7 pb-7">
               <span>Status</span>
-              <div class="cursor-pointer pt-1" @click="toggleSort">
+              <div class="itbkk-status-sort cursor-pointer pt-1" @click="toggleSort">
                 <template v-if="sortStatus === 0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -365,7 +378,7 @@ onMounted(fetchData)
               <p
                 class="itbkk-status rounded-2xl m-1 p-2 font-bold font-serif"
                 :class="{
-                  'bg-gray-200': task.statusName === 'No Status',
+                  'bg-gray-200 w-24': task.statusName === 'No Status',
                   'text-yellow-500': task.statusName === 'To Do',
                   'text-orange-400': task.statusName === 'Doing',
                   'text-green-400': task.statusName === 'Done',
@@ -376,12 +389,10 @@ onMounted(fetchData)
             </td>
             <td>
               <div class="itbkk-button-action relative pl-[20%]">
-                <img
-                  src="@/assets/optionIcon.svg"
-                  alt="Options"
-                  @click="toggleDropDown(index)"
-                  class="cursor-pointer"
-                />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 cursor-pointer" @click="toggleDropDown(index)">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
                 <div
                   v-if="optionsDropDownIndex === index"
                   class="absolute w-32 bg-white border rounded-lg shadow-lg z-50"
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import taskTable from '../components/taskTable.vue'
 import taskDetail from '../components/taskDetail.vue'
 import taskAdd from '@/components/taskAdd.vue'
-import taskEdit from '@/components/taskUpdate.vue'
+import taskEdit from '@/components/taskEdit.vue'
 import taskStatus from '@/components/statuses/StatusTable.vue'
 import statusAdd from '@/components/statuses/statusAdd.vue'
 import statusEdit from '@/components/statuses/statusEdit.vue'
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHistory("/nw3/"),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory("/nw3/"),
   routes: [
     {
       path: '/task',
